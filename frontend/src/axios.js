aaecd6710702681a87a971b6c9cbd1d98029a285
23A95A1203 <ravitejakatraju73@gmail.com>
2025-06-22 20:05:11 +0530

    Added backend url

--- frontend/src/axios.js
+++ frontend/src/axios.js
@@ -2,7 +2,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:5000/api',
+  baseURL: 'https://matrimony-connect.onrender.com/',
   withCredentials: true, // required for cookie auth
 });
 
