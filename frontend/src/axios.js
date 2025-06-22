e713902f82d837be45b27adcaa8fcd5f460dc4fc
23A95A1203 <ravitejakatraju73@gmail.com>
2025-06-22 09:29:02 +0530

    Updated apis

--- frontend/src/axios.js
+++ frontend/src/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-  baseURL: 'http://localhost:5000', // ✅ backend base
+   baseURL: process.env.REACT_APP_API_URL, // ✅ backend base
 });
 
 export default instance;
